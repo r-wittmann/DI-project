4a7d6e836f8ea50c2a75460111356a39606a7c7a
Rainer Wittmann <r-wittmann@outlook.de>
2021-12-15 14:01:02 +0100

    Backend: Implement rest of the /item endpoints (get, post, delete)

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -9,3 +9,37 @@ export const getItems = async (req, res) => {
         res.status(404).json({ message: error.message });
     }
 }
+
+export const createItem = async (req, res) => {
+    const item = req.body;
+    const newItem = new Item(item);
+
+    try {
+        await newItem.save();
+        res.status(201).json(newItem);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
+
+export const getItem = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const item = await Item.findById(id);
+        res.status(200).json(item);
+    } catch (e) {
+        res.status(404).json({ message: e.message });
+    }
+}
+
+export const deleteItem = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        await Item.findByIdAndDelete(id);
+        res.status(200).json({ message: "successfully deleted item"});
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
--- server/routes/items.js
+++ server/routes/items.js
@@ -1,8 +1,11 @@
 import express from "express";
-import {getItems} from "../controllers/items.js";
+import {createItem, deleteItem, getItem, getItems} from "../controllers/items.js";
 
 const router = express.Router();
 
 router.get('/', getItems);
+router.post("/create", createItem);
+router.get("/:id", getItem);
+router.delete("/:id", deleteItem);
 
 export default router;
